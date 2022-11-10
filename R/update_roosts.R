575c68e9cc5ad2772b4f4bc4d0f1a0250478891b
Kristen Dybala <kdybala@gmail.com>
2022-11-10 15:47:38 -0800

    make unsuitable land cover classes and proportion threshold more customizable in update_roosts

--- R/update_roosts.R
+++ R/update_roosts.R
@@ -7,14 +7,26 @@
 #' @details For landscapes that represent a projected change from baseline
 #'   conditions, this function facilitates evaluating historical crane roosts to
 #'   determine whether the land cover overlaying them in `landscape` is
-#'   projected to become unsuitable (>20% cover with an unsuitable land cover
-#'   class, including orchard, vineyard, riparian, woodland, scrub, urban).
-#'   Unsuitable roost polygons are removed, and updated roost maps are
-#'   generated, named as `scenario_name` in `pathout`. Use this function prior
-#'   to using [python_dist] to calculate distance to roost and generate updated
-#'   versions of `droost_km.tif` for each scenario.
+#'   projected to become unsuitable, based on exceeding a threshold `proportion`
+#'   covered by an unsuitable land cover class. Unsuitable roost polygons are
+#'   removed, and updated roost maps are generated, named as `scenario_name` in
+#'   `pathout`. Use this function prior to using [python_dist] to calculate
+#'   distance to roost and generate updated versions of `droost_km.tif` for each
+#'   scenario.
+#'
+#'   The default values for `unsuitable` include the original encodings for
+#'   orchard, vineyard, riparian, woodland, scrub, and urban, and the default
+#'   threshold value for `proportion` is 0.2. Alternate values can be provided
+#'   as desired.
 #'
 #' @param landscape SpatRaster created by [terra::rast()]
+#' @param unsuitable optional vector of numerical values representing the land
+#'   cover classifications that should be considered incompatible with crane
+#'   roosts; default values for the original land cover encoding include
+#'   orchard, vineyard, riparian, woodland, scrub, urban
+#' @param proportion numerical value for the proportion cover by an unsuitable
+#'   land cover class at which the roost should be considered unsuitable; see
+#'   Details
 #' @param roostpath Filepath to a raster representing the location of
 #'   traditional crane roosts.
 #' @param pathout Character string; Filepath to directory where output rasters
@@ -31,14 +43,15 @@
 #' @examples
 #' # See vignette
 
-update_roosts = function(landscape, roostpath, pathout, landscape_name,
-                         overwrite = FALSE) {
+update_roosts = function(landscape, unsuitable = c(11:19, 60, 70:79, 100:120),
+                         proportion = 0.2, roostpath, pathout,
+                         landscape_name, overwrite = FALSE) {
 
   # check how much traditional roosts overlap with incompatible land covers:
   # orchard, vineyard, riparian, woodland, scrub, urban
   roost_overlay = landscape %>%
-    terra::subst(from = c(11:19, 60, 70:79, 100:120), to = 1) %>%
-    terra::subst(from = c(2:130), to = 0) %>% #everything else
+    terra::subst(from = unsuitable, to = 1) %>%
+    terra::subst(from = c(2:999), to = 0) %>% #everything else
     terra::extract(terra::vect(roostpath))
 
   # identify polygons to exclude with >20% incompatible landcover
@@ -49,7 +62,7 @@ update_roosts = function(landscape, roostpath, pathout, landscape_name,
     dplyr::group_by(ID) %>%
     dplyr::mutate(ncell = sum(n), prop = n/ncell) %>%
     dplyr::ungroup() %>%
-    dplyr::filter(landscape == 1 & prop > 0.2) %>%
+    dplyr::filter(landscape == 1 & prop > proportion) %>%
     dplyr::arrange(dplyr::desc(prop))
 
   create_directory(file.path(pathout, landscape_name))
