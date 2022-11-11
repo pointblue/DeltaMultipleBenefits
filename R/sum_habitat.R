f1e10f7a0609312f353a56c5f9a3cd4fbd0453b8
Kristen Dybala <kdybala@gmail.com>
2022-11-11 12:20:46 -0800

    add rollup option to sum_habitat

--- R/sum_habitat.R
+++ R/sum_habitat.R
@@ -7,8 +7,8 @@
 #' @details This function will calculate the sum of all pixel values in each
 #'   raster found in `pathin` and any subdirectories. The file structure within
 #'   `pathin` is used to infer the name of the corresponding `SDM` and
-#'   `landscape_name`. This function is designed to efficiently
-#'   process multiple rasters located in the `pathin` directory at once.
+#'   `landscape_name`. This function is designed to efficiently process multiple
+#'   rasters located in the `pathin` directory at once.
 #'
 #'   If provided, `keypath` should be a CSV containing the fields `spp` and
 #'   `label`, used for converting the filenames of the rasters within `pathin`
@@ -23,6 +23,8 @@
 #'   encoding zones within which pixel values should be summarized
 #' @param subtype Character string appended to the field METRIC_SUBTYPE, such as
 #'   for distinguishing probability of presence from presence/absence
+#' @param rollup Logical; If `TRUE` (default), summarize total habitat across
+#'   all species/groups by set of SDMs
 #' @param keypath Optional filepath passed to [readr::read_csv()] translating
 #'   the individual species names as encoded in the file names in pathin to
 #'   METRIC names in the output table
@@ -35,7 +37,7 @@
 #' # See vignette
 
 sum_habitat = function(pathin, zonepath = NULL, subtype = 'distributions',
-                       keypath = NULL) {
+                       rollup = TRUE, keypath = NULL) {
   fl = list.files(pathin, '.tif$', recursive = TRUE, full.names = TRUE) %>%
     rlang::set_names()
 
@@ -61,15 +63,49 @@ sum_habitat = function(pathin, zonepath = NULL, subtype = 'distributions',
   res = res %>%
     dplyr::mutate(pathin = gsub(!!pathin, '', pathin),
            pathin = gsub('^\\/|.tif$', '', pathin)) %>%
-    tidyr::separate(pathin, sep = '/', into = c('SDM', 'scenario', 'spp')) %>%
-    dplyr::mutate(
-      METRIC_CATEGORY = 'Biodiversity Support',
-      METRIC_SUBTYPE = dplyr::case_when(
-        SDM == 'riparian' ~ paste('Riparian landbird', subtype),
-        SDM %in% c('waterbird_fall', 'waterbird_win') ~
-          paste('Waterbird', subtype)),
-      SCORE_TOTAL = value / .09, # convert to ha
-      UNIT = 'ha')
+    tidyr::separate(pathin, sep = '/', into = c('SDM', 'scenario', 'spp'))
+
+  if (rollup) {
+    # first find max value across all rasters for a scenario and SDM, then sum over landscape
+
+    totals = purrr::pmap_df(
+      res %>% dplyr::select(SDM, scenario) %>% dplyr::distinct(),
+      function(SDM, scenario) {
+        combined = list.files(file.path(pathin, SDM, scenario),
+                              '.tif$', full.names = TRUE) %>%
+          terra::rast() %>% max(na.rm = TRUE)
+        if (is.null(zonepath)) {
+          terra::values(combined) %>% sum(na.rm = TRUE) %>%
+            dplyr::as_tibble()
+        } else {
+          terra::zonal(combined, terra::rast(zonepath), 'sum', na.rm = TRUE) %>%
+            dplyr::as_tibble() %>%
+            rlang::set_names(c('ZONE', 'value'))
+        }
+      }) %>%
+      dplyr::bind_cols(res %>%
+                         dplyr::select(SDM, dplyr::any_of('ZONE'), scenario) %>%
+                         dplyr::distinct() %>% select(-any_of('ZONE'))) %>%
+      mutate(spp = 'TOTAL')
+
+      res = dplyr::bind_rows(res, totals)
+
+  }
+
+  # if (rollup) {
+  #   # summarize total habitat for the group, by SDM
+  #   res = dplyr::bind_rows(
+  #     res,
+  #     res %>%
+  #       dplyr::group_by(
+  #         across(
+  #           any_of(
+  #             c('SDM', 'scenario', 'ZONE', 'METRIC_CATEGORY', 'METRIC_SUBTYPE',
+  #               'UNIT')))) %>%
+  #       dplyr::summarize(SCORE_TOTAL = sum(SCORE_TOTAL),
+  #                        .groups = 'drop') %>%
+  #       dplyr::mutate(spp = 'TOTAL'))
+  # }
 
   if (!is.null(keypath)) {
     res = res %>%
@@ -81,12 +117,20 @@ sum_habitat = function(pathin, zonepath = NULL, subtype = 'distributions',
     res = dplyr::rename(res, METRIC = spp)
   }
 
-  res %>%
+  res = res %>%
     dplyr::mutate(
+      METRIC_CATEGORY = 'Biodiversity Support',
+      METRIC_SUBTYPE = dplyr::case_when(
+        SDM == 'riparian' ~ paste('Riparian landbird', subtype),
+        SDM %in% c('waterbird_fall', 'waterbird_win') ~
+          paste('Waterbird', subtype)),
       METRIC = dplyr::case_when(
         SDM == 'waterbird_fall' ~ paste0(METRIC, ' (fall)'),
         SDM == 'waterbird_win' ~ paste0(METRIC, ' (winter)'),
-        TRUE ~ METRIC)) %>%
+        TRUE ~ METRIC),
+      SCORE_TOTAL = value / .09, # convert to ha
+      UNIT = 'ha') %>%
     dplyr::select(scenario, any_of('ZONE'), METRIC_CATEGORY, METRIC_SUBTYPE,
                   METRIC, UNIT, SCORE_TOTAL)
+
 }
