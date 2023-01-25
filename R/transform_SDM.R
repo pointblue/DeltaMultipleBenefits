6ce1bf9b01e9184fae528aec5c4f0af4c8ecee17
Kristen Dybala <kdybala@gmail.com>
2023-01-25 15:09:46 -0800

    make SDM a parameter for consistency

--- R/transform_SDM.R
+++ R/transform_SDM.R
@@ -4,30 +4,31 @@
 #' species presence, such as resulting from [fit_SDM()], to binary predictions
 #' of presence or absence.
 #'
-#' @param pathin,landscape_name Character strings defining the filepath
-#'   (`pathin/landscape_name`) containing the predicted probability of presence
-#'   resulting from each distribution model, such as those created from running
-#'   [fit_SDM()]
+#' @param pathin,SDM,landscape_name Character strings defining the filepath
+#'   (`pathin/SDM/landscape_name`) containing the predicted probability of
+#'   presence resulting from each distribution model, such as those created from
+#'   running [fit_SDM()]; SDM must be one of `"riparian"`, `"waterbird_fall"`,
+#'   or `"waterbird_win"`.
 #' @param modlist List of model objects of class 'gbm' representing the
 #'   distribution models to which new predictors should be fit.
 #' @param stat Character string defining the threshold statistic to be used; see
 #'   [dismo::threshold()] for options
 #' @param pathout Filepath for the directory where results rasters should be
-#'   written
+#'   written (`pathout/SDM/landscape_name`)
 #' @param overwrite Logical; passed to [terra::writeRaster()]
 #'
-#' @return Nothing returned to R environment. Writes rasters to `pathout` for
-#'   each model in `modlist`
+#' @return Nothing returned to R environment. Writes rasters to
+#'   `pathout/SDM/landscape_name` for each model in `modlist`
 #' @seealso [fit_SDM()]
 #' @export
 #'
 #' @examples
 #' # See vignette
 
-transform_SDM = function(pathin, landscape_name, modlist, stat, pathout,
+transform_SDM = function(pathin, SDM, landscape_name, modlist, stat, pathout,
                          overwrite = FALSE) {
 
-  predictions = list.files(file.path(pathin, landscape_name),
+  predictions = list.files(file.path(pathin, SDM, landscape_name),
                            pattern = '.tif$', full.names = TRUE) %>%
     terra::rast()
 
@@ -49,7 +50,8 @@ transform_SDM = function(pathin, landscape_name, modlist, stat, pathout,
                 rcl = matrix(c(-Inf, threshold_list[[.x]], 0,
                                threshold_list[[.x]], Inf, 1),
                              nrow = 2, byrow = TRUE),
-                filename = paste0(file.path(pathout, landscape_name, .x), '.tif'),
+                filename = paste0(file.path(pathout, SDM, landscape_name, .x),
+                                  '.tif'),
                 overwrite = overwrite,
                 wopt = list(names = .x))
               )
