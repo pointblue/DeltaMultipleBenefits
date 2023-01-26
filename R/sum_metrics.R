f19fff6d824ce35b1df1514b25270dd14b09c772
Kristen Dybala <kdybala@gmail.com>
2023-01-25 22:39:14 -0800

    add line break to warning message

--- R/sum_metrics.R
+++ R/sum_metrics.R
@@ -75,7 +75,7 @@ sum_metrics = function(metricdat, areadat) {
   # check for missing land covers (since we did a full join)
   tmp = dat_join %>% dplyr::filter(is.na(.data$area))
   if (nrow(tmp) > 0) {
-    warning('Area estimates missing from "areadat" for one or more land cover classes in "metricdat"')
+    warning('\n\nArea estimates missing from "areadat" for one or more land cover classes in "metricdat"')
   }
 
   res = dplyr::bind_rows(
