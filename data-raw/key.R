9d2221df2d23487ca19a02776bbec9c6fcf4813a
Kristen Dybala <kdybala@gmail.com>
2026-05-19 17:50:40 -0700

    update documentation of veg key, replacing `CODE_BASELINE` and `CODE_NAME` with `CODE_NUM` and `NAME_FULL`

--- data-raw/key.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `key` dataset goes here
-
-key = readxl::read_excel('C:/Users/kdybala/Documents/R_projects/delta_tima/dat_clean/veg_key.xlsx') |>
-  dplyr::select(CODE_BASELINE, CODE_NAME, CLASS, SUBCLASS, DETAIL, LABEL, COLOR) |>
-  dplyr::arrange(CODE_BASELINE)
-
-usethis::use_data(key, overwrite = TRUE)
