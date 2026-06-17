55705ee91432e12ede9e6bc5ddf50e49335ce1fe
Kristen Dybala <kdybala@gmail.com>
2026-06-17 12:27:54 -0700

    update metrics to include SE = 0 where the value = 0, to better distinguish N loading which has no SE values

--- data-raw/metrics.R
+++ data-raw/metrics.R
@@ -1,5 +1,6 @@
 ## code to prepare `metrics` dataset goes here
 
-metrics = readr::read_csv('C:/Users/kdybala/Documents/R_projects/delta_scenarios/output/metrics.csv')
+#metrics = readr::read_csv('C:/Users/kdybala/Documents/R_projects/delta_scenarios/output/metrics.csv')
+metrics = readr::read_csv('C:/Users/kdybala/Documents/R_projects/delta_tima/dat_clean/metrics.csv')
 
 usethis::use_data(metrics, overwrite = TRUE)
