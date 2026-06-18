ce596f7ba60f5008b51c5130658d158f5bb32590
Kristen Dybala <kdybala@gmail.com>
2026-06-18 12:08:40 -0700

    add support for suffix appended to output files

--- inst/python/focal_stats.py
+++ inst/python/focal_stats.py
@@ -1,21 +1,28 @@
 # import arcpy
 # from arcpy import env
 # from arcpy.sa import *
+import os
+import arcpy
+from arcpy.sa import FocalStatistics, NbrCircle
 
-def focal_stats(fullpathin, fullpathout, buffer, fun = 'SUM', regex = '*'):
+def focal_stats(fullpathin, fullpathout, buffer, suffix, fun = 'SUM', regex = '*'):
   # Set environment settings
-  env.workspace = fullpathin
+  arcpy.env.workspace = fullpathin
 
   # Get a list of the matching rasters in the workspace
-  rast = arcpy.ListRasters(regex)
-
+  rasters = arcpy.ListRasters(regex)
+  
+  if not os.path.exists(fullpathout):
+    os.makedirs(fullpathout)
+  
   # Set out folder
-  outFolder = fullpathout
+  #outFolder = fullpathout
   
   # Loop through the list of rasters
-  for inRaster in rast:
-    # Set the outputname for each output to be the same as the input
-    outRaster = outFolder + "\\" + inRaster
+  for inRaster in rasters:
+    base, ext = os.path.splitext(inRaster)
+    out_name = base + suffix + ext
+    outRaster = os.path.join(fullpathout, out_name)
 
     # Process focal stats ("DATA" argument means NA values will be dropped)
     outFocalStat = FocalStatistics(inRaster, NbrCircle(buffer, "Map"), fun, "DATA")
