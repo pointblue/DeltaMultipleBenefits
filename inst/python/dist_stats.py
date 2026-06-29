63fff1c0be5eb358e58ab8a6e2ea8efff85e04e4
Kristen Dybala <kdybala@gmail.com>
2026-06-29 11:39:54 -0700

    align with focal_stats.py

--- inst/python/dist_stats.py
+++ inst/python/dist_stats.py
@@ -1,13 +1,19 @@
 # import arcpy
 # from arcpy import env  
 # from arcpy.sa import *  
+import os
+import arcpy
+from arcpy.sa import EucDistance
 
 def dist_stats(fullpathin, filename, fullpathout):
+  
   # Set environment settings
-  env.workspace = fullpathin
+  arcpy.env.workspace = os.path.abspath(fullpathin)
+  #env.workspace = fullpathin
   
   # Calculate Euclidean Distance
   outEucDistance = EucDistance(in_source_data = filename)
   
   # Save
+  fullpathout = os.path.abspath(fullpathout)
   outEucDistance.save(fullpathout)
